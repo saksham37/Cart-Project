5bbbd58ab06edb4847b3b0d82b692856f04952cc
saksham37 <sharma.ibn7@gmai.com>
2022-11-24 00:38:34 +0530

    add increaseQuantity function

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CartItem from './CartItem';
+function App() {
+  return (
+    <div className="App">
+      <h1>Meri cart</h1> 
+      <CartItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CartItem.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+class CartItem extends React.Component{
+    // state of a component stores the local/temporary data for a particular component
+    constructor(){
+        super();
+        this.state = {
+            title: "Mobile bas",
+            price: "99",
+            qty: "2"
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity = () => {
+        // by using the arrow function it will use the binding of the parent which is the CarItem 
+        console.log("test increaseQuantity");
+        console.log("this.state ",this.state);
+    }
+    render(){
+        const {title,price,qty} = this.state;
+        return (
+            <div className="cart-item"> 
+                <div style={styles.image} className="left-block">
+                    <img alt="Cart Item"/>
+                </div>
+                <div className="right-block">
+                    <div style={{fontSize:25}}> {title} </div>
+                    <div style={{color:'#777'}}> {price} </div>
+                    <div> Qty:{qty}  </div>
+                      <div className="cart-item-actions">
+                            {/* Here we will have buttons with className = "action-icons" */}
+                            <img alt="increase"
+                             className="action-icons"
+                             onClick = {this.increaseQuantity}
+                             src="https://cdn-icons-png.flaticon.com/128/992/992651.png"/>
+
+                            <img alt="decrease" 
+                            className="action-icons"
+                             src="https://cdn-icons-png.flaticon.com/128/992/992683.png"/>
+
+                            <img alt="delete" 
+                            className="action-icons"
+                             src="https://cdn-icons-png.flaticon.com/128/2907/2907762.png"/>
+                      </div>
+                </div>
+            </div>
+        );
+    }
+}
+const styles = {
+    image:{
+        height: 150,
+        width: 200,
+        marginRight: 10,
+        backgroundColor: '#ccc',
+        borderRadius: 5
+    }
+}
+export default CartItem;
